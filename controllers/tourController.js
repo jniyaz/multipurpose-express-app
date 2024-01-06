d4a6c1c2bac3683ea33398503c3fe445e8145fb4
Niyaz <niyaz.jm@icarasia.com>
2024-01-06 16:26:34 +0800

    add top 5 tours feature middleware

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -13,6 +13,14 @@ const Tour = require('../models/tourModel');
 //   next();
 // };
 
+// middlewares
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = 5;
+  req.query.sort = '-ratingsAverage, price';
+  req.query.fields = 'name, price, ratingsAverage, summary, difficulty';
+  next();
+};
+
 // GET TOURS
 exports.getTours = async (req, res) => {
   try {
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -5,9 +5,12 @@ const {
   createTour,
   updateTour,
   deleteTour,
+  aliasTopTours,
 } = require('../controllers/tourController');
 
 const router = express.Router();
+
+router.route('/top-5').get(aliasTopTours, getTours);
 
 router.route('/').get(getTours).post(createTour);
 
