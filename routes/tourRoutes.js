e6daa18f8e04e87cdcc77fcc447e2cd504127368
Niyaz <niyaz.jm@icarasia.com>
2024-01-06 20:22:07 +0800

    add tours stats feature with mongo  aggregation pipeline

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,11 +6,14 @@ const {
   updateTour,
   deleteTour,
   aliasTopTours,
+  getTourStats,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 
 router.route('/top-5').get(aliasTopTours, getTours);
+
+router.route('/tour-stats').get(getTourStats);
 
 router.route('/').get(getTours).post(createTour);
 
